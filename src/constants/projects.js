34ddc609e8c33ec01a77395aeeacd6cdb3a2d911
bhushanpatil288 <sharewithbhushan@gmail.com>
2026-04-19 12:10:02 +0530

    setup projects to be independent from react

---
+++ src/constants/projects.js
@@ -0,0 +1,9 @@
+import digitalClock from "/projects/phase-1-basics/day-1/digitalClock.html?url"
+
+export const projects = [
+    {
+        id: 1,
+        name: "Digital Clock",
+        path: digitalClock
+    }
+]
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,13 @@
 import { Container } from "../components"
+import { projects } from "../constants/projects"
 
 const Projects = () => {
     return (
         <Container>
             <div className="min-h-screen flex items-center justify-center">
-                Projects
+                {projects.map((project) => (
+                    <a key={project.id} href={project.path} target="_blank" className="bg-lime-500 px-6 py-2 rounded-2xl shadow hover:bg-lime-600 transition-all">{project.name}</a>
+                ))}
             </div>
         </Container>
     )
